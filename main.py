61469b3fc6e6a4d324690c874b450535dece84a2
Nick Powers <nickpowers43@gmail.com>
2016-11-06 15:49:46 -0500

    Initial model setup

--- main.py
+++ main.py
@@ -29,16 +29,39 @@ try:
     printvar('M',M)
     printvar('N',N)
 
-    # # Create a new model
-    # m = Model("model0")
-    #
-    #
+    # Create a new model
+    m = Model("model0")
+
+    z = []
+    zSum = LinExpr(0.0)
+    for m in range(M):
+        zRow = []
+        for t in range(T):
+            idle = m.addVar(lb=0.0, ub=1.0, vtype=GRB.CONTINUOUS, name='Z_m{0}_t{1}'.format(m, t))
+            zSum.addTerms(1.0, idle)
+            zRow.append(idle)
+        z.append(zRow)
+
+    m.update()
+    m.setObjective(zSum, GRB.MINIMIZE)
+
+    x = []
+    for m in range(M):
+        jobStartTimes = []
+        for j in range(len(length[m])):
+            thisJobStarts = []
+            for t in range(T):
+                thisJobStarts.append(m.addVar(lb=0.0, ub=1.0, vtype=GRB.CONTINUOUS, name='X_m{0}_j{1}_t{2}'.format(m, j, t)))
+            jobStartTimes.append(thisJobStarts)
+        x.append(jobStartTimes)
+
+
 
     # # Create variables
     # x = m.addVar(vtype=GRB.CONTINUOUS, name="x")
     # y = m.addVar(vtype=GRB.CONTINUOUS, name="y")
     #
-    # # Integrate new variables
+    # Integrate new variables
     # m.update()
     #
     # # Set objective
@@ -47,9 +70,9 @@ try:
     # # Add constraint: x + 2 y + 3 z <= 4
     # # m.addConstr(2 * x + 3 * y == 5, "c0")
     # # m.addRange(x + 0, 0.0, 1.0, "c1")
-    #
-    # m.optimize()
-    #
+
+    m.optimize()
+
     # for v in m.getVars():
     #     print('%s %g' % (v.varName, v.x))
     #
